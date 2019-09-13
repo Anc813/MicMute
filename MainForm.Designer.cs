d99959ee5f07bdfcbda7f3dae17877bfe84b21bf
Pavek B <anc813@gmail.com>
2019-09-14 00:29:53 +0300

    Add add hotkey ability

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -29,9 +29,14 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.icon = new System.Windows.Forms.NotifyIcon(this.components);
             this.iconContextMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.hotkeyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.hotkeyTextBox = new Shortcut.Forms.HotkeyTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonReset = new System.Windows.Forms.Button();
             this.iconContextMenu.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -45,28 +50,72 @@
             // iconContextMenu
             // 
             this.iconContextMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.hotkeyToolStripMenuItem,
             this.toolStripMenuItem1});
             this.iconContextMenu.Name = "iconContextMenu";
-            this.iconContextMenu.Size = new System.Drawing.Size(93, 26);
+            this.iconContextMenu.Size = new System.Drawing.Size(113, 48);
+            // 
+            // hotkeyToolStripMenuItem
+            // 
+            this.hotkeyToolStripMenuItem.Name = "hotkeyToolStripMenuItem";
+            this.hotkeyToolStripMenuItem.Size = new System.Drawing.Size(112, 22);
+            this.hotkeyToolStripMenuItem.Text = "Hotkey";
+            this.hotkeyToolStripMenuItem.Click += new System.EventHandler(this.HotkeyToolStripMenuItem_Click);
             // 
             // toolStripMenuItem1
             // 
             this.toolStripMenuItem1.Name = "toolStripMenuItem1";
-            this.toolStripMenuItem1.Size = new System.Drawing.Size(92, 22);
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(112, 22);
             this.toolStripMenuItem1.Text = "Exit";
             this.toolStripMenuItem1.Click += new System.EventHandler(this.ExitMenuItem_Click);
+            // 
+            // hotkeyTextBox
+            // 
+            this.hotkeyTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.hotkeyTextBox.Hotkey = null;
+            this.hotkeyTextBox.Location = new System.Drawing.Point(12, 38);
+            this.hotkeyTextBox.Name = "hotkeyTextBox";
+            this.hotkeyTextBox.Size = new System.Drawing.Size(181, 26);
+            this.hotkeyTextBox.TabIndex = 1;
+            this.hotkeyTextBox.Text = "None";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(275, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Register hotkey (auto saved on close)";
+            // 
+            // buttonReset
+            // 
+            this.buttonReset.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonReset.Location = new System.Drawing.Point(205, 39);
+            this.buttonReset.Name = "buttonReset";
+            this.buttonReset.Size = new System.Drawing.Size(77, 26);
+            this.buttonReset.TabIndex = 3;
+            this.buttonReset.Text = "reset";
+            this.buttonReset.UseVisualStyleBackColor = true;
+            this.buttonReset.Click += new System.EventHandler(this.ButtonReset_Click);
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(163, 78);
+            this.ClientSize = new System.Drawing.Size(294, 77);
+            this.Controls.Add(this.buttonReset);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.hotkeyTextBox);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "MainForm";
-            this.Text = "Form1";
-            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.MainForm_FormClosed);
+            this.Text = "MicMute";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
             this.Load += new System.EventHandler(this.MainForm_Load);
             this.iconContextMenu.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -75,6 +124,10 @@
         private System.Windows.Forms.NotifyIcon icon;
         private System.Windows.Forms.ContextMenuStrip iconContextMenu;
         private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private Shortcut.Forms.HotkeyTextBox hotkeyTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ToolStripMenuItem hotkeyToolStripMenuItem;
+        private System.Windows.Forms.Button buttonReset;
     }
 }
 
--- MainForm.cs
+++ MainForm.cs
@@ -1,68 +1,23 @@
-﻿using System;
-using System.Diagnostics;
-using System.Drawing;
-using System.Runtime.InteropServices;
-using System.Windows.Forms;
-using System.Reactive.Linq;
-using AudioSwitcher.AudioApi;
+﻿using AudioSwitcher.AudioApi;
 using AudioSwitcher.AudioApi.CoreAudio;
+using Microsoft.Win32;
+using Shortcut;
+using System;
+using System.Drawing;
+using System.Reactive.Linq;
+using System.Windows.Forms;
 
 
 namespace MicMute
 {
     public partial class MainForm : Form
     {
-        #region Hook
-
-        private const int WH_KEYBOARD_LL = 13;
-        private const int WM_KEYDOWN = 0x0100;
-        private static LowLevelKeyboardProc _proc = HookCallback;
-        private static IntPtr _hookID = IntPtr.Zero;
-
-        private static IntPtr SetHook(LowLevelKeyboardProc proc)
-        {
-            using (Process curProcess = Process.GetCurrentProcess())
-            using (ProcessModule curModule = curProcess.MainModule)
-            {
-                return SetWindowsHookEx(WH_KEYBOARD_LL, proc,
-                    GetModuleHandle(curModule.ModuleName), 0);
-            }
-        }
-
-        private delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
-
-        private static IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
-        {
-            if (nCode >= 0 && wParam == (IntPtr)WM_KEYDOWN)
-            {
-                int vkCode = Marshal.ReadInt32(lParam);
-                // MessageBox.Show(vkCode.ToString());
-
-                if (vkCode == 180)
-                { // use LauchMail media key
-                    MicMute.Program.mf.ToggleMicStatus();
-                }
-            }
-
-            return CallNextHookEx(_hookID, nCode, wParam, lParam);
-        }
-
-        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
-        private static extern IntPtr SetWindowsHookEx(int idHook, LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
-
-        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
-        [return: MarshalAs(UnmanagedType.Bool)]
-        private static extern bool UnhookWindowsHookEx(IntPtr hhk);
-
-        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
-        private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
-
-        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
-        private static extern IntPtr GetModuleHandle(string lpModuleName);
-
-        #endregion Hook
-
         public CoreAudioController AudioController = new CoreAudioController();
+        private readonly HotkeyBinder hotkeyBinder = new HotkeyBinder();
+        private readonly RegistryKey registryKey = Registry.CurrentUser.CreateSubKey(@"SOFTWARE\MicMute");
+        private readonly string registryKeyName = "Hotkey";
+
+        private Hotkey hotkey;
 
         public MainForm()
         {
@@ -74,17 +29,34 @@ namespace MicMute
             UpdateDevice(next.Device);
         }
 
+        private void MyHide()
+        {
+            ShowInTaskbar = false;
+            Location = new Point(-10000, -10000);
+            Visible = false;
+        }
+
+        private void MyShow()
+        {
+            Visible = true;
+            ShowInTaskbar = true;
+            CenterToScreen();
+        }
+
         private void MainForm_Load(object sender, EventArgs e)
         {
-            _hookID = SetHook(_proc);
-
-            Form form = (Form)sender;
-            form.ShowInTaskbar = false;
-            form.Location = new Point(-10000, -10000);
-            form.Visible = false;
-
+            MyHide();
             UpdateDevice(AudioController.DefaultCaptureDevice);
             AudioController.AudioDeviceChanged.Subscribe(OnNextDevice);
+
+            var hotkeyValue = registryKey.GetValue(registryKeyName);
+            if (hotkeyValue != null)
+            {
+                var converter = new Shortcut.Forms.HotkeyConverter();
+                hotkey = (Hotkey)converter.ConvertFromString(hotkeyValue.ToString());
+                hotkeyBinder.Bind(hotkey).To(ToggleMicStatus);
+            }
+
             //AudioController.AudioDeviceChanged.Subscribe(x =>
             //{
             //    Debug.WriteLine("{0} - {1}", x.Device.Name, x.ChangedType.ToString());
@@ -112,11 +84,11 @@ namespace MicMute
         {
             if (device != null)
             {
-                this.UpdateIcon(device.IsMuted ? iconOff : iconOn, device.FullName);
+                UpdateIcon(device.IsMuted ? iconOff : iconOn, device.FullName);
             }
             else
             {
-                this.UpdateIcon(iconError, "< No device >");
+                UpdateIcon(iconError, "< No device >");
             }
         }
         private void UpdateIcon(Icon icon, string tooltipText)
@@ -136,17 +108,12 @@ namespace MicMute
 
             if (device != null)
             {
-                this.UpdateIcon(device.IsMuted ? Properties.Resources.off : Properties.Resources.on, device.FullName);
+                UpdateIcon(device.IsMuted ? Properties.Resources.off : Properties.Resources.on, device.FullName);
             }
             else
             {
-                this.UpdateIcon(Properties.Resources.error, "< No device >");
+                UpdateIcon(Properties.Resources.error, "< No device >");
             }
-        }
-
-        private void ExitMenuItem_Click(object sender, EventArgs e)
-        {
-            Application.Exit();
         }
 
         private void Icon_MouseClick(object sender, MouseEventArgs e)
@@ -157,9 +124,42 @@ namespace MicMute
             }
         }
 
-        private void MainForm_FormClosed(object sender, FormClosedEventArgs e)
+        private void HotkeyToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            UnhookWindowsHookEx(_hookID);
+            if (hotkey != null)
+            {
+                hotkeyTextBox.Hotkey = hotkey;
+                hotkeyBinder.Unbind(hotkey);
+            }
+            MyShow();
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            MyHide();
+            e.Cancel = true;
+
+            hotkey = hotkeyTextBox.Hotkey;
+
+            if (hotkey == null)
+            {
+                registryKey.DeleteValue(registryKeyName);
+            }
+            else
+            {
+                hotkeyBinder.Bind(hotkey).To(ToggleMicStatus);
+                registryKey.SetValue(registryKeyName, hotkey);
+            }
+        }
+
+        private void ButtonReset_Click(object sender, EventArgs e)
+        {
+            hotkeyTextBox.Hotkey = null;
+            hotkeyTextBox.Text = "None";
+        }
+        private void ExitMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
         }
     }
 }
